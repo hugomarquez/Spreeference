287cf088efa53ef361aebaea768c5220bd886f54
hugomarquez <hugomarquez.dev@gmail.com>
2017-02-19 19:46:58 -0600

    adds travis cli and dummy app

---
+++ spec/dummy/app/models/app_configuration.rb
@@ -0,0 +1,6 @@
+class AppConfiguration < Spreeference::Configuration
+  # preference :checkbox, :boolean,  default: true
+  # preference :input, :string, default:'changeme'
+  # preference :number, :integer, default: 123
+  # preference :list, :array, default:[]
+end
---
+++ spec/dummy/config/initializers/spreeference.rb
@@ -0,0 +1,13 @@
+# Configure Spree Preferences
+#
+# Note: If a preference is set here it will be stored within the cache & database upon initialization.
+#       Just removing an entry from this initializer will not make the preference value go away.
+#       Instead you must either set a new value or remove entry, clear cache, and remove database entry.
+#
+# In order to initialize a setting do:
+# config.setting_name = 'new value'
+Spreeference.config do |config|
+  # Example:
+  # Uncomment to stop tracking inventory levels in the application
+  config.is_ok = 'Nice!'
+end
